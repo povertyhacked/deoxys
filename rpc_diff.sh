3682c13d5f89ce78affcebf2dbc3456deee6af18
sparqet <sparqet@gmail.com>
2023-07-19 14:56:44 +0200

    added test to rpc diff

--- rpc_diff.sh
+++ rpc_diff.sh
@@ -120,3 +120,236 @@ for field in $fields; do
         echo "$diff_output"
     fi
 done
+
+
+echo "////////////////////////////////////////////GET_STATE_UPDATE//////////////////////////////////////////"
+
+# Faire la première requête curl
+response1=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getStateUpdate","params":[{"block_number": 9100}],"id":0}' \
+     http://127.0.0.1:9956/ | jq '.')
+
+# Faire la deuxième requête curl
+response2=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getStateUpdate","params":[{"block_number": 9100}],"id":0}' \
+     https://starknet-mainnet.g.alchemy.com/v2/hnj_DGevqpyoyeoEs9Vfx-6qSTHOnaIu | jq '.')
+
+# Extraire les champs pour comparaison
+fields=$(echo "$response1" | jq -r '.result | keys[]')
+
+# Comparer les champs et afficher les différences
+diff_output=""
+
+for field in $fields; do
+    value1=$(echo "$response1" | jq -r ".result.$field")
+    value2=$(echo "$response2" | jq -r ".result.$field")
+    diff_output=$(diff -u <(echo "$value1") <(echo "$value2") | colordiff)
+    if [ -z "$diff_output" ]; then
+        echo ""
+    else
+        echo "$field"
+        echo "$diff_output"
+    fi
+done
+
+
+echo "////////////////////////////////////////////GET_BLOCK_WITH_TX_HASHES//////////////////////////////////////////"
+
+# Faire la première requête curl
+response1=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getBlockWithTxHashes","params":[{"block_number": 9100}],"id":0}' \
+     http://127.0.0.1:9956/ | jq '.')
+
+# Faire la deuxième requête curl
+response2=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getBlockWithTxHashes","params":[{"block_number": 9100}],"id":0}' \
+     https://starknet-mainnet.g.alchemy.com/v2/hnj_DGevqpyoyeoEs9Vfx-6qSTHOnaIu | jq '.')
+
+# Extraire les champs pour comparaison
+fields=$(echo "$response1" | jq -r '.result | keys[]')
+
+# Comparer les champs et afficher les différences
+diff_output=""
+
+for field in $fields; do
+    value1=$(echo "$response1" | jq -r ".result.$field")
+    value2=$(echo "$response2" | jq -r ".result.$field")
+    diff_output=$(diff -u <(echo "$value1") <(echo "$value2") | colordiff)
+    if [ -z "$diff_output" ]; then
+        echo ""
+    else
+        echo "$field"
+        echo "$diff_output"
+    fi
+done
+
+
+
+echo "////////////////////////////////////////////GET_CLASS_HASH_AT//////////////////////////////////////////"
+
+# Faire la première requête curl
+response1=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getClassHashAt","params":[
+      {
+        "block_number": 2344
+      },
+      "0x057c5f8f9e18e5fd5d81a33e11f4144e77d854634a0505680a3012c163551f12"
+    ],"id":0}' \
+     http://127.0.0.1:9956/ | jq '.')
+
+# Faire la deuxième requête curl
+response2=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getClassHashAt","params":[
+      {
+        "block_number": 2344
+      },
+      "0x057c5f8f9e18e5fd5d81a33e11f4144e77d854634a0505680a3012c163551f12"
+    ],"id":0}' \
+     https://starknet-mainnet.g.alchemy.com/v2/hnj_DGevqpyoyeoEs9Vfx-6qSTHOnaIu | jq '.')
+
+# Extraire les champs pour comparaison
+fields=$(echo "$response1" | jq -r '.result | keys[]')
+
+# Comparer les champs et afficher les différences
+diff_output=""
+
+for field in $fields; do
+    value1=$(echo "$response1" | jq -r ".result.$field")
+    value2=$(echo "$response2" | jq -r ".result.$field")
+    diff_output=$(diff -u <(echo "$value1") <(echo "$value2") | colordiff)
+    if [ -z "$diff_output" ]; then
+        echo ""
+    else
+        echo "$field"
+        echo "$diff_output"
+    fi
+done
+
+
+
+echo "////////////////////////////////////////////GET_NONCE//////////////////////////////////////////"
+
+# Faire la première requête curl
+response1=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getNonce","params":[
+      {
+        "block_number": 2344
+      },
+      "0x057c5f8f9e18e5fd5d81a33e11f4144e77d854634a0505680a3012c163551f12"
+    ],"id":0}' \
+     http://127.0.0.1:9956/ | jq '.')
+
+# Faire la deuxième requête curl
+response2=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getNonce","params":[
+      {
+        "block_number": 2344
+      },
+      "0x057c5f8f9e18e5fd5d81a33e11f4144e77d854634a0505680a3012c163551f12"
+    ],"id":0}' \
+     https://starknet-mainnet.g.alchemy.com/v2/hnj_DGevqpyoyeoEs9Vfx-6qSTHOnaIu | jq '.')
+
+# Extraire les champs pour comparaison
+fields=$(echo "$response1" | jq -r '.result | keys[]')
+
+# Comparer les champs et afficher les différences
+diff_output=""
+
+for field in $fields; do
+    value1=$(echo "$response1" | jq -r ".result.$field")
+    value2=$(echo "$response2" | jq -r ".result.$field")
+    diff_output=$(diff -u <(echo "$value1") <(echo "$value2") | colordiff)
+    if [ -z "$diff_output" ]; then
+        echo ""
+    else
+        echo "$field"
+        echo "$diff_output"
+    fi
+done
+
+
+
+echo "////////////////////////////////////////////SYNCING//////////////////////////////////////////"
+
+# Faire la première requête curl
+response1=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_syncing","id":0}' \
+     http://127.0.0.1:9956/ | jq '.')
+
+# Faire la deuxième requête curl
+response2=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_syncing","id":0}' \
+     https://starknet-mainnet.g.alchemy.com/v2/hnj_DGevqpyoyeoEs9Vfx-6qSTHOnaIu | jq '.')
+
+# Extraire les champs pour comparaison
+fields=$(echo "$response1" | jq -r '.result | keys[]')
+
+# Comparer les champs et afficher les différences
+diff_output=""
+
+for field in $fields; do
+    value1=$(echo "$response1" | jq -r ".result.$field")
+    value2=$(echo "$response2" | jq -r ".result.$field")
+    diff_output=$(diff -u <(echo "$value1") <(echo "$value2") | colordiff)
+    if [ -z "$diff_output" ]; then
+        echo ""
+    else
+        echo "$field"
+        echo "$diff_output"
+    fi
+done
+
+
+
+echo "////////////////////////////////////////////GET_CLASS_AT//////////////////////////////////////////"
+
+# Faire la première requête curl
+response1=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getClassAt","params":[
+      {
+        "block_number": 2344
+      },
+      "0x057c5f8f9e18e5fd5d81a33e11f4144e77d854634a0505680a3012c163551f12"
+    ],"id":0}' \
+     http://127.0.0.1:9956/ | jq '.')
+
+# Faire la deuxième requête curl
+response2=$(curl -s -X POST \
+     -H "Content-Type: application/json" \
+     --data '{"jsonrpc":"2.0","method":"starknet_getClassAt","params":[
+      {
+        "block_number": 2344
+      },
+      "0x057c5f8f9e18e5fd5d81a33e11f4144e77d854634a0505680a3012c163551f12"
+    ],"id":0}' \
+     https://starknet-mainnet.g.alchemy.com/v2/hnj_DGevqpyoyeoEs9Vfx-6qSTHOnaIu | jq '.')
+
+# Extraire les champs pour comparaison
+fields=$(echo "$response1" | jq -r '.result | keys[]')
+
+# Comparer les champs et afficher les différences
+diff_output=""
+
+for field in $fields; do
+    value1=$(echo "$response1" | jq -r ".result.$field")
+    value2=$(echo "$response2" | jq -r ".result.$field")
+    diff_output=$(diff -u <(echo "$value1") <(echo "$value2") | colordiff)
+    if [ -z "$diff_output" ]; then
+        echo ""
+    else
+        echo "$field"
+        echo "$diff_output"
+    fi
+done
+
